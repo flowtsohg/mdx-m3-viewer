581a6ecc626e9ea6b27e592d762a23d641369dc0
Chananya Freiman <spectralcanine@gmail.com>
2015-10-10 23:31:18 +0300

    Whoops

--- examples/objmodelinstance.js
+++ examples/objmodelinstance.js
@@ -6,4 +6,6 @@ OBJModelInstance.prototype = Object.create(BaseModelInstance.prototype);
 
 OBJModelInstance.prototype.loadstart = function (asyncInstance, reportError, reportLoad) {
     BaseModelInstance.call(this, asyncInstance.asyncModel.model, {});
+
+    reportLoad();
 };
