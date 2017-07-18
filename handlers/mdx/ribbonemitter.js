dd8b7764295a9419c9a90181517b9bb1630228c8
Chananya Freiman <spectralcanine@gmail.com>
2017-07-18 04:24:01 +0300

    Oops

--- handlers/mdx/ribbonemitter.js
+++ handlers/mdx/ribbonemitter.js
@@ -58,7 +58,29 @@ MdxRibbonEmitter.prototype = {
 
     update: MdxParticleEmitter.prototype.update,
     updateData: MdxParticle2Emitter.prototype.updateData,
-    render: MdxParticle2Emitter.prototype.render,
+
+    render(bucket, shader) {
+        let active = this.active.length;
+
+        if (active > 0) {
+            let model = this.model,
+                gl = model.gl;
+
+            this.layer.bind(shader);
+
+            gl.uniform2fv(shader.uniforms.get("u_dimensions"), this.dimensions);
+
+            model.bindTexture(this.texture, 0, bucket.modelView);
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer.buffer);
+            gl.bufferSubData(gl.ARRAY_BUFFER, 0, this.buffer.float32array.subarray(0, active * 30));
+
+            gl.vertexAttribPointer(shader.attribs.get("a_position"), 3, gl.FLOAT, false, 20, 0);
+            gl.vertexAttribPointer(shader.attribs.get("a_uva_rgb"), 2, gl.FLOAT, false, 20, 12);
+
+            gl.drawArrays(gl.TRIANGLES, 0, active * 6);
+        }
+    },
 
     shouldRender(instance) {
         return this.getVisibility(instance) > 0.75;
