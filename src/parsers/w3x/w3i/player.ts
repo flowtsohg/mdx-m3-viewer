bae0fbdda372fef8154ad12ff4f1e0996fb65543
trigger <triggerhappy187@live.com>
2020-04-03 12:33:39 -0700

    Update W3I
    - Tested on 1.32.3.
    - Added buildVersion, scriptMode, and graphicsMode.
    - Player now takes format version so it can properly parse the latest format without breaking backwards compability.

--- src/parsers/w3x/w3i/player.ts
+++ src/parsers/w3x/w3i/player.ts
@@ -4,6 +4,8 @@ import BinaryStream from '../../../common/binarystream';
  * A player.
  */
 export default class Player {
+  private version: number = 0;
+
   id: number = 0;
   type: number = 0;
   race: number = 0;
@@ -12,16 +14,24 @@ export default class Player {
   startLocation: Float32Array = new Float32Array(2);
   allyLowPriorities: number = 0;
   allyHighPriorities: number = 0;
+  unknown1: Uint8Array = new Uint8Array(8);
+
+  constructor(version: number) {
+    this.version = version;
+  }
 
   load(stream: BinaryStream) {
     this.id = stream.readInt32();
     this.type = stream.readInt32();
     this.race = stream.readInt32();
-    this.isFixedStartPosition = stream.readInt32();
+    this.isFixedStartPosition = stream.readInt32();;
     this.name = stream.readUntilNull();
     stream.readFloat32Array(this.startLocation);
     this.allyLowPriorities = stream.readUint32();
     this.allyHighPriorities = stream.readUint32();
+    if (this.version > 30) {
+      stream.readUint8Array(this.unknown1);
+    }
   }
 
   save(stream: BinaryStream) {
@@ -36,6 +46,6 @@ export default class Player {
   }
 
   getByteLength() {
-    return 33 + this.name.length;
+    return 41 + this.name.length;
   }
 }
