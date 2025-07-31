1de67e94b703ce51d67f4497e629c68072bf2ecf
Huy Pham <huypham060398@gmail.com>
2025-07-31 05:14:30 +0300

    setup @pqhuy/mdx-m3-viewer

--- clients/shared/localorhive.js
+++ clients/shared/localorhive.js
@@ -3,8 +3,8 @@ import urlWithParams from '../../src/common/urlwithparams';
 export default function localOrHive(src, params) {
   src = src.toLowerCase();
 
-  if (window.location.hostname === '127.0.0.1') {
-    return urlWithParams(`${window.location.origin}/assets?path=${src}`, params);
+  if (window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost') {
+    return urlWithParams(`${window.location.origin}/assets/${src}`, params);
   } else {
     // if (params && (params.reforged || params.sc2)) {
     return `https://www.hiveworkshop.com/casc-contents?path=${src}`;
